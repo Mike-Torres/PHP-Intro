ea373bb34ebe53cebee126c101ed54cbcd3914e0
Michael Torres <michaelj.torres@yahoo.com>
2019-01-06 22:17:35 -0500

    Intro to PHP server side

---
+++ index.php
@@ -0,0 +1,76 @@
+
+<!--
+<form method="GET">
+  <input type="text" name="person">
+  <button>Submit</button>
+</form>
+-->
+<?php
+/*
+ $name = $_GET['person'];
+ echo  $name." is a handsome fellow <br> <br>"; // br has to go within strings
+echo "Mike is awesome";
+---------------------------------------------------------------------------------------------------------------
+In house Function Methods
+
+echo strlen("Hi Mike"); // string length - PHP function
+echo "<br>";
+echo "<br>";
+echo str_word_count("Hi Mike"); // String words amount - PHP function
+echo "<br>";
+echo "<br>";
+echo strrev("Hi Mike"); // String Reverse - PHP function
+echo "<br>";
+echo "<br>";
+echo strpos("Hi Mike", "Mike"); // indexing
+echo "<br>";
+echo "<br>";
+echo str_replace("Hi Mike", "Mike","Daniel"); // indexing
+--------------------------------------------------------------------------------------------------------------------
+                                                      DATA TYPES
+//String
+$name = "coding is fun"; // single/double quotes JS rules still apply
+
+//integers
+$name = 20; // integer
+$name = 20.53123; // float has decimals
+
+/*booleans
+true = 1;
+false = 0; */
+/*
+array
+$names = array("Daniel","Mike","JIMBOB");
+echo $names['0'];
+*/
+
+
+
+/* Arithmetic Operators
+  echo 5+5;  Same as JS
+  echo 8%5;  Remainder - same as JS
+  echo 5**5;  Squaring
+ */
+//---------------------------------------------------------------------------------------------------------------------------------------------
+/*                                                     Assignment Operators
+
+$x = 100;
+$x += 20;
+
+echo $x;
+*/
+
+// ---------------------------------------------------------------------------------------------------------------------
+//                                                       Comparison Operators
+$x = 5;
+$y = 10;
+
+
+if($x === $y) {
+  echo "true!";
+} else {
+  echo "false";
+}
+
+
+?>
